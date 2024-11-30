73db7784d5b698f24b3bc1e64d3c32ea48b7d0a8
Ilya Filchagov <41166473+rocksteady24@users.noreply.github.com>
2024-11-30 12:03:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,10 +34,10 @@ rm glpi-${SOFTWARE_VERSION_TAG}.tgz
 chown -R www-data:www-data ../glpi-files
 
 # Create the systemd service file
-SERVICE_FILE="/etc/systemd/system/glpi.service"
+SERVICE_FILE="/lib/systemd/system/glpi.service"
 echo "\
 [Unit]
-Description=GLPI Docker Compose Service
+Description=GLPI Service
 After=docker.service
 Requires=docker.service
 Conflicts=shutdown.target reboot.target halt.target
@@ -53,20 +53,17 @@ ExecStop=/usr/bin/docker compose -f glpi.yml down
 WantedBy=multi-user.target
 " > "$SERVICE_FILE"
 
+# Created link to glpi.service file
+ln -s /lib/systemd/system/glpi.service /etc/systemd/system/multi-user.target.wants/glpi.service
+
 # Reload systemd to recognize the new service
 if ! systemctl daemon-reload; then
     echo "Error: Failed to reload systemd."
     exit 1
 fi
 
-# Start the GLPI service
-if ! systemctl start glpi; then
-    echo "Error: Failed to start GLPI service."
-    exit 1
-fi
-
 # Enable the GLPI service to start on boot
-if ! systemctl enable glpi; then
+if ! systemctl enable --now glpi.service; then
     echo "Error: Failed to enable GLPI service."
     exit 1
 fi
